40ef83700db4830c5edff11570357b539affcff8
Thomas Byttebier <thomasbyttebier@MacBook-Pro-van-Thomas.local>
2020-02-17 22:46:55 +0100

    Removed web fonts

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-var fontA = new FontFaceObserver('Tisa', {
-    weight: 400
-});
-
-var fontB = new FontFaceObserver('Tisa', {
-    weight: 700
-});
-
-Promise.all([fontA.load(), fontB.load()]).then(function () {
-  document.documentElement.className += " js-fonts-loaded";
-});
-
 function onBlur() {
     document.body.className = 'js-is-blurred';
 };
